b166be00d2b9b178d65b01fe83acb97836272c84
Ishtiaq uddin <ishty119@gmail.com>
2022-05-22 18:16:56 +0600

    verify admin setup

--- index.js
+++ index.js
@@ -16,29 +16,46 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
-function verifyJWT(req, res, next) {
-    const authHeader = req.headers.authorization;
-    if (!authHeader) {
-      return res.status(401).send({ message: "UnAuthorized access" });
-    }
-    const token = authHeader.split(" ")[1];
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
-      if (err) {
-        return res.status(403).send({ message: "Forbidden access" });
-      }
-      req.decoded = decoded;
-      next();
-    });
-  }
+
 
 async function run() {
   try {
     await client.connect();
     const userCollection = client.db("assignment12").collection("users");
 
+    const verifyAdmin = async (req,res,next)=>{
+        const requester = req.decoded.email;
+        const requesterAccount = await userCollection.findOne({email:requester})
+        if(requesterAccount.role==="admin"){
+            next();
+        }
+        else{
+            res.status(403).send({message: "forbidden"});
+        }
+    };
+
     app.get("/user", async (req, res) => {
         const users = await userCollection.find().toArray();
         res.send(users);
+      });
+      app.get("/admin/:email", async (req, res) => {
+        const email = req.params.email;
+        const user = await userCollection.findOne({ email: email });
+        const isAdmin = user.role === "admin";
+        res.send({ admin: isAdmin });
+      });
+  
+      app.put("/user/admin/:email" , verifyAdmin,async (req, res) => {
+        const email = req.params.email;
+  
+        const filter = { email: email };
+  
+        const updateDoc = {
+          $set: { role: "admin" },
+        };
+        const result = await userCollection.updateOne(filter, updateDoc);
+  
+        res.send(result);
       });
 
       app.put("/user/:email", async (req, res) => {
